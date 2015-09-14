63d44d313f9c838326785ea3ca3d5b4654e9c262
mlangasek <langasek@tectronik.cz>
2015-09-14 07:00:46 +0200

    [WP8] Add support for local folder
    If file wasnt found then path is assumed to be relative path to local directory

--- src/wp8/FileOpener2.cs
+++ src/wp8/FileOpener2.cs
@@ -17,26 +17,48 @@ namespace WPCordovaClassLib.Cordova.Commands
         public async void open(string options)
         {
             string[] args = JSON.JsonHelper.Deserialize<string[]>(options);
-
-            string aliasCurrentCommandCallbackId = args[2];
-
+            this.openPath(args[0].Replace("/", "\\"), args[2]);
+            string[] args = JSON.JsonHelper.Deserialize<string[]>(options);
+        }
+        
+        private async void openPath(string path, string cbid)
+        {
             try
             {
-                // Get the file.
-                StorageFile file = await Windows.Storage.StorageFile.GetFileFromPathAsync(args[0]);
-
-                // Launch the bug query file.
+                StorageFile file = await Windows.Storage.StorageFile.GetFileFromPathAsync(path);
                 await Windows.System.Launcher.LaunchFileAsync(file);
-
-                DispatchCommandResult(new PluginResult(PluginResult.Status.OK), aliasCurrentCommandCallbackId);
+                DispatchCommandResult(new PluginResult(PluginResult.Status.OK), cbid);
             }
             catch (FileNotFoundException)
             {
-                DispatchCommandResult(new PluginResult(PluginResult.Status.IO_EXCEPTION), aliasCurrentCommandCallbackId);
+                this.openInLocalFolder(path, cbid);
             }
             catch (Exception)
             {
-                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR), aliasCurrentCommandCallbackId);
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR), cbid);
+            }
+        }
+        
+        private async void openInLocalFolder(string path, string cbid)
+        {
+            try
+            {
+                StorageFile file = await Windows.Storage.StorageFile.GetFileFromPathAsync(
+                    Path.Combine(
+                        Windows.Storage.ApplicationData.Current.LocalFolder.Path,
+                        path.TrimStart('\\')
+                    )
+                );
+                await Windows.System.Launcher.LaunchFileAsync(file);
+                DispatchCommandResult(new PluginResult(PluginResult.Status.OK), cbid);
+            }
+            catch (FileNotFoundException)
+            {
+                DispatchCommandResult(new PluginResult(PluginResult.Status.IO_EXCEPTION), cbid);
+            }
+            catch (Exception)
+            {
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR), cbid);
             }
         }
     }
